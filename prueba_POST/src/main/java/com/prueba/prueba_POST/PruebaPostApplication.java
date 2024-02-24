ad404e9f3f8eb7b2acc1979c1bca04935239337d
Julian Pignani <pignanijulian@gmail.com>
2024-02-24 10:58:43 -0300

    Creando nuevo proyecto para realizar practicas con POST

---
+++ prueba_POST/src/main/java/com/prueba/prueba_POST/PruebaPostApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.prueba_POST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPostApplication.class, args);
+	}
+
+}
---
+++ prueba_POST/src/test/java/com/prueba/prueba_POST/PruebaPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.prueba_POST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
